09d3aa91b94eb02ef7643d3db9335fca58b4d849
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-07-18 18:44:49 -0400

    feat(): add http tests

--- internal/server/http_test.go
+++ internal/server/http_test.go
@@ -3,12 +3,20 @@ package server
 import (
 	"bytes"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+func TestNewHTTPServer(t *testing.T) {
+	server := NewHTTPServer()
+	if server == nil {
+		t.Error("Expected pointer to initialized HTTPServer, got nil instead")
+	}
+}
 
 func TestSetHTTP(t *testing.T) {
 	server := NewHTTPServer()
@@ -39,10 +47,18 @@ func TestSetHTTP(t *testing.T) {
 		},
 	}
 
+	type request struct {
+		Value string `json:"value"`
+	}
+
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			url := fmt.Sprintf("/SET/%s", tc.key)
-			byteData := []byte(base64.StdEncoding.EncodeToString(tc.value))
+			reqData := request{base64.StdEncoding.EncodeToString(tc.value)}
+			byteData, err := json.Marshal(reqData)
+			if err != nil {
+				t.Errorf("Failed to encode data into JSON: %v", err)
+			}
 			res, err := sendRequestHTTP("PUT", url, bytes.NewReader(byteData), server)
 			if err != nil {
 				t.Errorf("Failed to send request: %v", err)
@@ -55,29 +71,138 @@ func TestSetHTTP(t *testing.T) {
 }
 
 func TestGetHTTP(t *testing.T) {
+	server := NewHTTPServer()
 
+	testCases := []struct {
+		name          string
+		key           string
+		ok            bool
+		expectedValue []byte
+		expectedCode  int
+	}{
+		{
+			name:         "Invalid key, no value",
+			key:          "",
+			expectedCode: http.StatusNotFound,
+		},
+		{
+			name:          "Valid key, no value",
+			key:           "key1",
+			ok:            false,
+			expectedValue: nil,
+			expectedCode:  http.StatusOK,
+		},
+		{
+			name:          "Valid key, valid value",
+			key:           "key1",
+			ok:            true,
+			expectedValue: []byte("10"),
+			expectedCode:  http.StatusOK,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.key != "" {
+				server.cache.Set(tc.key, tc.expectedValue)
+			}
+
+			url := fmt.Sprintf("/GET/%s", tc.key)
+			res, err := sendRequestHTTP("GET", url, nil, server)
+			if err != nil {
+				t.Errorf("Failed to send request: %v", err)
+			}
+			if code := res.Result().StatusCode; code != tc.expectedCode {
+				t.Errorf("Expected response status code %d, got %d", tc.expectedCode, code)
+			}
+
+			resData := httpResponse{}
+			json.NewDecoder(res.Body).Decode(&resData)
+			val, ok := resData.Value.(string)
+			if tc.expectedValue != nil && !ok {
+				t.Error("Not ok")
+			}
+			if bytes.Compare([]byte(val), tc.expectedValue) != 0 {
+				t.Errorf("Expected value %v, instead got %v", tc.expectedValue, []byte(val))
+			}
+		})
+	}
 }
 
 func TestDeleteHTTP(t *testing.T) {
+	server := NewHTTPServer()
+	server.cache.Set("key1", []byte("10"))
+	server.cache.Set("key2", []byte("20"))
+	server.cache.Set("key3", []byte("30"))
+	lengthBefore := server.cache.Length()
 
+	url := fmt.Sprintf("/DELETE/%s", "key1")
+	res, err := sendRequestHTTP("DELETE", url, nil, server)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	if code := res.Result().StatusCode; code != http.StatusOK {
+		t.Errorf("Expected response status code %d, got %d", http.StatusOK, code)
+	}
+	if server.cache.Length() != lengthBefore-1 {
+		t.Errorf("Cache length has not changed")
+	}
 }
 
 func TestFlushHTTP(t *testing.T) {
+	server := NewHTTPServer()
+	server.cache.Set("key1", []byte("10"))
+	server.cache.Set("key2", []byte("20"))
+	server.cache.Set("key3", []byte("30"))
 
+	res, err := sendRequestHTTP("DELETE", "/FLUSH", nil, server)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	if code := res.Result().StatusCode; code != http.StatusOK {
+		t.Errorf("Expected response status code %d, got %d", http.StatusOK, code)
+	}
+	if server.cache.Length() != 0 {
+		t.Errorf("Cache is not empty")
+	}
 }
 
 func TestLengthHTTP(t *testing.T) {
+	server := NewHTTPServer()
+	server.cache.Set("key1", []byte("10"))
+	server.cache.Set("key2", []byte("20"))
+	server.cache.Set("key3", []byte("30"))
+	server.cache.Set("key4", []byte("40"))
+	server.cache.Set("key5", []byte("50"))
 
+	res, err := sendRequestHTTP("GET", "/LENGTH", nil, server)
+	if err != nil {
+		t.Errorf("Failed to send request: %v", err)
+	}
+	if code := res.Result().StatusCode; code != http.StatusOK {
+		t.Errorf("Expected response status code %d, got %d", http.StatusOK, code)
+	}
+
+	resData := httpResponse{}
+	json.NewDecoder(res.Body).Decode(&resData)
+	val, ok := resData.Value.(float64)
+	if !ok {
+		t.Error("Returned value is not a number")
+	}
+	actualLength := server.cache.Length()
+	if int(val) != actualLength {
+		t.Errorf("Expected length %d, got %d", actualLength, int(val))
+	}
 }
 
 func TestPingHTTP(t *testing.T) {
 	server := NewHTTPServer()
 	res, err := sendRequestHTTP("GET", "/PING", nil, server)
 	if err != nil {
-		t.Errorf("Failed on GET \"/PING\" request: %v", err)
+		t.Errorf("Failed to send request: %v", err)
 	}
-	if code := res.Result().StatusCode; code != 200 {
-		t.Errorf("Expected response status code 200, got %v", code)
+	if code := res.Result().StatusCode; code != http.StatusOK {
+		t.Errorf("Expected response status code %d, got %d", http.StatusOK, code)
 	}
 }
 

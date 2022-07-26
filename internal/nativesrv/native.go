e748306722d2335fe9d857df682242a9eda2aea4
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-07-25 20:41:22 -0400

    wip(): add atomicBool to nativesrc pkg, expand Server struct

--- internal/nativesrv/native.go
+++ internal/nativesrv/native.go
@@ -1,17 +1,30 @@
 package nativesrv
 
-import "net"
+import (
+	"net"
+	"sync"
+	"sync/atomic"
 
-type Server struct{}
+	"github.com/nmezhenskyi/rcs/internal/cache"
+)
+
+type Server struct {
+	addr  string
+	cache *cache.CacheMap
+
+	inShutdown atomicBool
+
+	mu sync.Mutex
+}
 
 // --- Public API: --- //
 
 func (s *Server) ListenAndServe(addr string) error {
-	lis, err := net.Listen("tcp", addr)
+	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		return err
 	}
-	return s.serve(lis)
+	return s.serve(listener)
 }
 
 func (s *Server) Shutdown() {
@@ -24,6 +37,33 @@ func (s *Server) Close() {
 
 // --- Private: --- //
 
-func (s *Server) serve(lis net.Listener) error {
-	return nil
+func (s *Server) serve(listener net.Listener) error {
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+		go s.handleConnection(conn)
+	}
+}
+
+func (s *Server) handleConnection(conn net.Conn) {
+
+}
+
+// --- Helpers: --- //
+
+type atomicBool int32
+
+func (b *atomicBool) isSet() bool {
+	return atomic.LoadInt32((*int32)(b)) != 0
+}
+
+func (b *atomicBool) setTrue() {
+	atomic.StoreInt32((*int32)(b), 1)
+}
+
+func (b *atomicBool) setFalse() {
+	atomic.StoreInt32((*int32)(b), 0)
 }

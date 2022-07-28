0f004ac0fcbac577f2f914d0a70ed7dd9f9211e2
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-07-28 19:13:20 -0400

    feat(main): add main Logger instance, read config

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,55 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"os"
 
 	"github.com/nmezhenskyi/rcs/internal/httpsrv"
+	"github.com/rs/zerolog"
 )
 
 func main() {
+	// Main logger instance.
+	var logger = zerolog.New(os.Stderr).With().Timestamp().
+		Logger().Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	configFile := flag.String("c", "rcs.json", "Configuration file")
+	devMode := flag.Bool("d", false, "Enable development mode")
+	flag.Parse()
+
+	_, err := os.Stat(*configFile)
+	if os.IsNotExist(err) {
+		logger.Fatal().Err(err).Msg("Configuration file is missing")
+	} else if err != nil {
+		logger.Fatal().Err(err).Msg("Failed to access configuration file")
+	}
+	conf, err := readConfig(*configFile)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Failed to parse configuration file")
+	}
+	if *devMode {
+		conf.Verbosity = "dev"
+	}
+
+	switch conf.Verbosity {
+	case "prod":
+		logger = zerolog.New(os.Stderr).
+			With().Timestamp().Logger().Level(zerolog.InfoLevel)
+	case "dev":
+		logger = zerolog.New(os.Stderr).
+			With().Timestamp().Logger().Level(zerolog.DebugLevel).
+			Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	case "none":
+		logger = zerolog.New(os.Stderr).
+			With().Timestamp().Logger().Level(zerolog.Disabled)
+	}
+
+	logger.Info().Msg("--- RCS Started ---")
+
 	httpServer := httpsrv.NewServer(nil)
-	log.Println("--- RCS: Start ---")
+	httpServer.Logger = logger.With().Str("scope", "http").Logger()
 
 	if err := httpServer.ListenAndServe("localhost:5000"); err != nil {
-		log.Fatal(err)
+		logger.Fatal().Err(err).Msg("")
 	}
 }

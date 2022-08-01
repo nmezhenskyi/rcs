a202dfaf78d8b31c98f8953e76dd82e4f4e55024
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-08-01 19:45:16 -0400

    wip(native): added logger, working on handleConnection

--- internal/nativesrv/native.go
+++ internal/nativesrv/native.go
@@ -1,22 +1,34 @@
 package nativesrv
 
 import (
+	"fmt"
 	"net"
+	"os"
 	"sync"
 	"sync/atomic"
 
 	"github.com/nmezhenskyi/rcs/internal/cache"
+	"github.com/rs/zerolog"
 )
 
+const (
+	MaxMessageSize     = 1048576 // 1 MB
+	DefaultMessageSize = MaxMessageSize
+)
+
+// Server implements RCS Native TCP Protocol.
+//
+// Accepts only TCP connections.
 type Server struct {
 	cache *cache.CacheMap
 
-	addr       string
 	inShutdown atomicBool
 
 	mu          sync.Mutex
 	listener    *srvListener
 	activeConns map[*net.TCPConn]struct{}
+
+	Logger zerolog.Logger // By defaut Logger is disabled, but can be manually attached.
 }
 
 // --- Public API: --- //
@@ -28,15 +40,21 @@ func NewServer(c *cache.CacheMap) *Server {
 	return &Server{
 		cache:       c,
 		activeConns: make(map[*net.TCPConn]struct{}),
+		Logger:      zerolog.New(os.Stderr).Level(zerolog.Disabled),
 	}
 }
 
 func (s *Server) ListenAndServe(addr string) error {
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
+		s.Logger.Error().Err(err).Msg("failed to start listener")
 		return err
 	}
-	return s.serve(listener)
+	err = s.serve(listener)
+	if err != nil {
+		s.Logger.Error().Err(err).Msg("failed while serving")
+	}
+	return err
 }
 
 func (s *Server) Shutdown() {
@@ -47,6 +65,7 @@ func (s *Server) Close() {
 	s.inShutdown.setTrue()
 	s.mu.Lock()
 	defer s.mu.Unlock()
+	s.listener.Close()
 	for c := range s.activeConns {
 		c.Close()
 		delete(s.activeConns, c)
@@ -62,16 +81,36 @@ func (s *Server) serve(lis net.Listener) error {
 	s.listener = lis.(*srvListener)
 	s.mu.Unlock()
 	for {
-		conn, err := lis.Accept()
+		newConn, err := lis.Accept()
 		if err != nil {
-			return err
+			s.Logger.Error().Err(err).Msg("failed to accept connection")
+			continue
 		}
-		go s.handleConnection(conn)
+		tcpConn, ok := newConn.(*net.TCPConn)
+		if !ok {
+			s.Logger.Info().Msg(fmt.Sprintf("rejected \"%s\" connection (%s)",
+				newConn.RemoteAddr().String(), newConn.RemoteAddr().Network()))
+			continue
+		}
+		s.mu.Lock()
+		s.activeConns[tcpConn] = struct{}{}
+		s.mu.Unlock()
+		go s.handleConnection(tcpConn)
 	}
 }
 
 func (s *Server) handleConnection(conn net.Conn) {
+	defer conn.Close()
 
+	for {
+		buf := make([]byte, DefaultMessageSize)
+		n, err := conn.Read(buf)
+		if n == 0 || err != nil {
+			s.Logger.Error().Err(err).Msg("connection read error")
+			return
+		}
+		// TODO: parse message, process, send response
+	}
 }
 
 func (s *Server) shuttingDown() bool {

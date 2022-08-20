1f5c4db020d678731d7844466090cc91523e0f67
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-08-19 22:14:16 -0400

    feat(native): extend SET command test

--- internal/nativesrv/native_test.go
+++ internal/nativesrv/native_test.go
@@ -37,8 +37,34 @@ func TestSet(t *testing.T) {
 			value: nil,
 			expectedResponse: response{
 				command: []byte("SET"),
-				message: []byte("Key and/or value are missing"),
+				message: []byte("Key is missing"),
 				ok:      false,
+				key:     nil,
+				value:   nil,
+			},
+		},
+		{
+			name:  "Empty key, empty value",
+			key:   []byte(""),
+			value: []byte(""),
+			expectedResponse: response{
+				command: []byte("SET"),
+				message: []byte("Key is missing"),
+				ok:      false,
+				key:     nil,
+				value:   nil,
+			},
+		},
+		{
+			name:  "Valid key, empty value",
+			key:   []byte("key1"),
+			value: []byte(""),
+			expectedResponse: response{
+				command: []byte("SET"),
+				message: []byte("Value is missing"),
+				ok:      false,
+				key:     []byte("key1"),
+				value:   nil,
 			},
 		},
 	}
@@ -57,30 +83,34 @@ func TestSet(t *testing.T) {
 			req.write(conn)
 
 			respBuf := [1024]byte{}
-			conn.Read(respBuf[:])
-			resp, err := parseResponse(respBuf[:])
+			n, err := conn.Read(respBuf[:])
 			if err != nil {
-				t.Errorf("Received invalid response: %v", err)
+				t.Errorf("Error while reading from server")
+			}
+			resp, err := parseResponse(respBuf[:n])
+			if err != nil {
+				t.Logf("Response buffer:\n%s", string(respBuf[:n]))
+				t.Logf("Error while parsing response: %v", err)
 			}
 
 			if resp.ok != tc.expectedResponse.ok {
-				t.Errorf("Expected ok to be %T, got %T instead",
+				t.Errorf("Expected ok to be \"%v\", got \"%v\" instead",
 					tc.expectedResponse.ok, resp.ok)
 			}
 			if bytes.Compare(resp.command, tc.expectedResponse.command) != 0 {
-				t.Errorf("Expected command to be %s, got %s instead",
+				t.Errorf("Expected command to be \"%s\", got \"%s\" instead",
 					string(tc.expectedResponse.command), string(resp.command))
 			}
 			if bytes.Compare(resp.message, tc.expectedResponse.message) != 0 {
-				t.Errorf("Expected message to be %s, got %s instead",
+				t.Errorf("Expected message to be \"%s\", got \"%s\" instead",
 					string(tc.expectedResponse.message), string(resp.message))
 			}
 			if bytes.Compare(resp.key, tc.expectedResponse.key) != 0 {
-				t.Errorf("Expected key to be %s, got %s instead",
+				t.Errorf("Expected key to be \"%s\", got \"%s\" instead",
 					string(tc.expectedResponse.key), string(resp.key))
 			}
 			if bytes.Compare(resp.value, tc.expectedResponse.value) != 0 {
-				t.Errorf("Expected value to be %s, got %s instead",
+				t.Errorf("Expected value to be \"%s\", got \"%s\" instead",
 					string(tc.expectedResponse.value), string(resp.value))
 			}
 		})

f2e13ceb57d1c781d547519e35fd187725d105be
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-08-21 19:06:35 -0400

    test(native): implement test for GET command

--- internal/nativesrv/native_test.go
+++ internal/nativesrv/native_test.go
@@ -156,7 +156,117 @@ func TestSet(t *testing.T) {
 }
 
 func TestGet(t *testing.T) {
+	server := NewServer(nil)
+	serverAddr := "localhost:5000"
+	go func() {
+		if err := server.ListenAndServe(serverAddr); err != nil {
+			t.Errorf("Server failed: %v", err)
+		}
+	}()
+	defer server.Close()
 
+	testCases := []struct {
+		name             string
+		key              []byte
+		payloadValue     []byte
+		expectedResponse response
+	}{
+		{
+			name: "Nil key",
+			key:  nil,
+			expectedResponse: response{
+				command: []byte("GET"),
+				message: []byte("Key is missing"),
+				ok:      false,
+				key:     nil,
+				value:   nil,
+			},
+		},
+		{
+			name: "Empty key",
+			key:  []byte(""),
+			expectedResponse: response{
+				command: []byte("GET"),
+				message: []byte("Key is missing"),
+				ok:      false,
+				key:     nil,
+				value:   nil,
+			},
+		},
+		{
+			name:         "Valid key, unnecessary value",
+			key:          []byte("key1"),
+			payloadValue: []byte("some_value"),
+			expectedResponse: response{
+				command: []byte("GET"),
+				message: []byte("Received unexpected value"),
+				ok:      false,
+				key:     []byte("key1"),
+				value:   nil,
+			},
+		},
+		{
+			name: "Valid key, valid value",
+			key:  []byte("key1"),
+			expectedResponse: response{
+				command: []byte("GET"),
+				message: nil,
+				ok:      true,
+				key:     []byte("key1"),
+				value:   []byte("some_value"),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if len(tc.key) != 0 && tc.expectedResponse.value != nil {
+				server.cache.Set(string(tc.key), tc.expectedResponse.value)
+			}
+			req := request{
+				command: []byte("GET"),
+				key:     tc.key,
+				value:   tc.payloadValue,
+			}
+			conn, err := net.Dial("tcp", serverAddr)
+			if err != nil {
+				t.Errorf("Failed to connect to the server: %v", err)
+			}
+			req.write(conn)
+
+			respBuf := [1024]byte{}
+			n, err := conn.Read(respBuf[:])
+			if err != nil {
+				t.Errorf("Error while reading from server")
+			}
+			resp, err := parseResponse(respBuf[:n])
+			if err != nil {
+				t.Logf("Response buffer:\n%s", string(respBuf[:n]))
+				t.Logf("Error while parsing response: %v", err)
+			}
+
+			if resp.ok != tc.expectedResponse.ok {
+				t.Errorf("Expected ok to be \"%v\", got \"%v\" instead",
+					tc.expectedResponse.ok, resp.ok)
+			}
+			if bytes.Compare(resp.command, tc.expectedResponse.command) != 0 {
+				t.Errorf("Expected command to be \"%s\", got \"%s\" instead",
+					string(tc.expectedResponse.command), string(resp.command))
+			}
+			if bytes.Compare(resp.message, tc.expectedResponse.message) != 0 {
+				t.Errorf("Expected message to be \"%s\", got \"%s\" instead",
+					string(tc.expectedResponse.message), string(resp.message))
+			}
+			if bytes.Compare(resp.key, tc.expectedResponse.key) != 0 {
+				t.Errorf("Expected key to be \"%s\", got \"%s\" instead",
+					string(tc.expectedResponse.key), string(resp.key))
+			}
+			if bytes.Compare(resp.value, tc.expectedResponse.value) != 0 {
+				t.Errorf("Expected value to be \"%s\", got \"%s\" instead",
+					string(tc.expectedResponse.value), string(resp.value))
+			}
+		})
+	}
 }
 
 func TestDelete(t *testing.T) {

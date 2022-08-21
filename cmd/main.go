a934609eb32b390d48a26aca19c9c1e988ad2321
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-08-21 19:50:02 -0400

    feat(main): proper init of cache & servers, graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/nmezhenskyi/rcs/internal/cache"
+	"github.com/nmezhenskyi/rcs/internal/grpcsrv"
 	"github.com/nmezhenskyi/rcs/internal/httpsrv"
+	"github.com/nmezhenskyi/rcs/internal/nativesrv"
 	"github.com/rs/zerolog"
 )
 
@@ -43,12 +51,61 @@ func main() {
 		logger = zerolog.New(os.Stderr).Level(zerolog.Disabled)
 	}
 
+	var (
+		globalCache  *cache.CacheMap
+		nativeServer *nativesrv.Server
+		httpServer   *httpsrv.Server
+		grpcServer   *grpcsrv.Server
+
+		shutdownSignal = make(chan os.Signal, 1)
+	)
+	signal.Notify(shutdownSignal, syscall.SIGINT, syscall.SIGTERM)
+
 	logger.Info().Msg("--- RCS Started ---")
 
-	httpServer := httpsrv.NewServer(nil)
-	httpServer.Logger = logger.With().Str("scope", "http").Logger()
+	globalCache = cache.NewCacheMap()
 
-	if err := httpServer.ListenAndServe("localhost:5000"); err != nil {
-		logger.Fatal().Err(err).Msg("")
+	if conf.Native.Activate {
+		nativeServer = nativesrv.NewServer(globalCache)
+		nativeServer.Logger = logger.With().Str("scope", "native").Logger()
+		// TODO: start native server
 	}
+	if conf.HTTP.Activate {
+		httpServer = httpsrv.NewServer(globalCache)
+		httpServer.Logger = logger.With().Str("scope", "http").Logger()
+		if conf.HTTP.TLS {
+			go httpServer.ListenAndServeTLS(
+				getLocalAddr(conf.HTTP.Port, conf.HTTP.OnLocalhost),
+				conf.HTTP.CertFile, conf.HTTP.KeyFile)
+		} else {
+			go httpServer.ListenAndServe(getLocalAddr(conf.HTTP.Port, conf.HTTP.OnLocalhost))
+		}
+	}
+	if conf.GRPC.Activate {
+		grpcServer = grpcsrv.NewServer(globalCache)
+		grpcServer.Logger = logger.With().Str("scope", "grpc").Logger()
+		// TODO: start grpc server
+	}
+
+	<-shutdownSignal
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if nativeServer != nil {
+		nativeServer.Shutdown(ctx)
+	}
+	if httpServer != nil {
+		httpServer.Shutdown(ctx)
+	}
+	if grpcServer != nil {
+		grpcServer.Shutdown(ctx)
+	}
+
+	logger.Info().Msg("--- RCS Stopped ---")
+}
+
+func getLocalAddr(port int, localhost bool) string {
+	if localhost {
+		return fmt.Sprintf("localhost:%d", port)
+	}
+	return fmt.Sprintf(":%d", port)
 }

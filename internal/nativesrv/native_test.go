5e85aa845ff320e040fcaf37a5bfd38eaf52e00a
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-08-22 19:56:30 -0400

    test(native): test all commands

--- internal/nativesrv/native_test.go
+++ internal/nativesrv/native_test.go
@@ -2,8 +2,12 @@ package nativesrv
 
 import (
 	"bytes"
+	"io"
 	"net"
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestNewServer(t *testing.T) {
@@ -270,25 +274,437 @@ func TestGet(t *testing.T) {
 }
 
 func TestDelete(t *testing.T) {
+	server := NewServer(nil)
+	serverAddr := "localhost:5000"
+	go func() {
+		if err := server.ListenAndServe(serverAddr); err != nil {
+			t.Errorf("Server failed: %v", err)
+		}
+	}()
+	defer server.Close()
 
+	testCases := []struct {
+		name             string
+		key              []byte
+		cacheValue       []byte
+		expectedResponse response
+	}{
+		{
+			name:       "Nil key",
+			key:        nil,
+			cacheValue: []byte("some_value"),
+			expectedResponse: response{
+				command: []byte("DELETE"),
+				message: []byte("Key is missing"),
+				ok:      false,
+				key:     nil,
+				value:   nil,
+			},
+		},
+		{
+			name: "Empty key",
+			key:  []byte(""),
+			expectedResponse: response{
+				command: []byte("DELETE"),
+				message: []byte("Key is missing"),
+				ok:      false,
+				key:     nil,
+				value:   nil,
+			},
+		},
+		{
+			name: "Valid key",
+			key:  []byte("key1"),
+			expectedResponse: response{
+				command: []byte("DELETE"),
+				message: nil,
+				ok:      true,
+				key:     []byte("key1"),
+				value:   nil,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if len(tc.key) != 0 && tc.expectedResponse.value != nil {
+				server.cache.Set(string(tc.key), tc.cacheValue)
+			}
+			req := request{
+				command: []byte("DELETE"),
+				key:     tc.key,
+			}
+			conn, err := net.Dial("tcp", serverAddr)
+			if err != nil {
+				t.Errorf("Failed to connect to the server: %v", err)
+			}
+			req.write(conn)
+
+			respBuf := [1024]byte{}
+			n, err := conn.Read(respBuf[:])
+			if err != nil {
+				t.Errorf("Error while reading from server")
+			}
+			resp, err := parseResponse(respBuf[:n])
+			if err != nil {
+				t.Logf("Response buffer:\n%s", string(respBuf[:n]))
+				t.Logf("Error while parsing response: %v", err)
+			}
+
+			if resp.ok != tc.expectedResponse.ok {
+				t.Errorf("Expected ok to be \"%v\", got \"%v\" instead",
+					tc.expectedResponse.ok, resp.ok)
+			}
+			if bytes.Compare(resp.command, tc.expectedResponse.command) != 0 {
+				t.Errorf("Expected command to be \"%s\", got \"%s\" instead",
+					string(tc.expectedResponse.command), string(resp.command))
+			}
+			if bytes.Compare(resp.message, tc.expectedResponse.message) != 0 {
+				t.Errorf("Expected message to be \"%s\", got \"%s\" instead",
+					string(tc.expectedResponse.message), string(resp.message))
+			}
+			if bytes.Compare(resp.key, tc.expectedResponse.key) != 0 {
+				t.Errorf("Expected key to be \"%s\", got \"%s\" instead",
+					string(tc.expectedResponse.key), string(resp.key))
+			}
+			if bytes.Compare(resp.value, tc.expectedResponse.value) != 0 {
+				t.Errorf("Expected value to be \"%s\", got \"%s\" instead",
+					string(tc.expectedResponse.value), string(resp.value))
+			}
+			if _, ok := server.cache.Get(string(resp.key)); ok {
+				t.Errorf("Value was not removed from Server.cache")
+			}
+		})
+	}
 }
 
 func TestPurge(t *testing.T) {
+	server := NewServer(nil)
+	serverAddr := "localhost:5000"
+	go func() {
+		if err := server.ListenAndServe(serverAddr); err != nil {
+			t.Errorf("Server failed: %v", err)
+		}
+	}()
+	defer server.Close()
 
+	expectedResponse := response{
+		command: []byte("PURGE"),
+		message: nil,
+		ok:      true,
+		key:     nil,
+		value:   nil,
+	}
+
+	server.cache.Set("key1", []byte("value1"))
+	server.cache.Set("key2", []byte("value2"))
+	server.cache.Set("key3", []byte("value3"))
+
+	req := request{command: []byte("PURGE")}
+	conn, err := net.Dial("tcp", serverAddr)
+	if err != nil {
+		t.Errorf("Failed to connect to the server: %v", err)
+	}
+	req.write(conn)
+
+	respBuf := [1024]byte{}
+	n, err := conn.Read(respBuf[:])
+	if err != nil {
+		t.Errorf("Error while reading from server")
+	}
+	resp, err := parseResponse(respBuf[:n])
+	if err != nil {
+		t.Logf("Response buffer:\n%s", string(respBuf[:n]))
+		t.Logf("Error while parsing response: %v", err)
+	}
+
+	if resp.ok != expectedResponse.ok {
+		t.Errorf("Expected ok to be \"%v\", got \"%v\" instead",
+			expectedResponse.ok, resp.ok)
+	}
+	if bytes.Compare(resp.command, expectedResponse.command) != 0 {
+		t.Errorf("Expected command to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.command), string(resp.command))
+	}
+	if bytes.Compare(resp.message, expectedResponse.message) != 0 {
+		t.Errorf("Expected message to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.message), string(resp.message))
+	}
+	if bytes.Compare(resp.key, expectedResponse.key) != 0 {
+		t.Errorf("Expected key to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.key), string(resp.key))
+	}
+	if bytes.Compare(resp.value, expectedResponse.value) != 0 {
+		t.Errorf("Expected value to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.value), string(resp.value))
+	}
+	if server.cache.Length() != 0 {
+		t.Error("Server.cache is not empty")
+	}
 }
 
 func TestLength(t *testing.T) {
+	server := NewServer(nil)
+	serverAddr := "localhost:5000"
+	go func() {
+		if err := server.ListenAndServe(serverAddr); err != nil {
+			t.Errorf("Server failed: %v", err)
+		}
+	}()
+	defer server.Close()
 
+	expectedResponse := response{
+		command: []byte("LENGTH"),
+		message: nil,
+		ok:      true,
+		key:     nil,
+		value:   []byte("5"),
+	}
+
+	server.cache.Set("key1", []byte("value1"))
+	server.cache.Set("key2", []byte("value2"))
+	server.cache.Set("key3", []byte("value3"))
+	server.cache.Set("key4", []byte("value4"))
+	server.cache.Set("key5", []byte("value5"))
+
+	req := request{command: []byte("LENGTH")}
+	conn, err := net.Dial("tcp", serverAddr)
+	if err != nil {
+		t.Errorf("Failed to connect to the server: %v", err)
+	}
+	req.write(conn)
+
+	respBuf := [1024]byte{}
+	n, err := conn.Read(respBuf[:])
+	if err != nil {
+		t.Errorf("Error while reading from server")
+	}
+	resp, err := parseResponse(respBuf[:n])
+	if err != nil {
+		t.Logf("Response buffer:\n%s", string(respBuf[:n]))
+		t.Logf("Error while parsing response: %v", err)
+	}
+
+	if resp.ok != expectedResponse.ok {
+		t.Errorf("Expected ok to be \"%v\", got \"%v\" instead",
+			expectedResponse.ok, resp.ok)
+	}
+	if bytes.Compare(resp.command, expectedResponse.command) != 0 {
+		t.Errorf("Expected command to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.command), string(resp.command))
+	}
+	if bytes.Compare(resp.message, expectedResponse.message) != 0 {
+		t.Errorf("Expected message to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.message), string(resp.message))
+	}
+	if bytes.Compare(resp.key, expectedResponse.key) != 0 {
+		t.Errorf("Expected key to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.key), string(resp.key))
+	}
+	if bytes.Compare(resp.value, expectedResponse.value) != 0 {
+		t.Errorf("Expected value to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.value), string(resp.value))
+	}
+	cacheLen := strconv.Itoa(server.cache.Length())
+	if cacheLen != string(expectedResponse.value) {
+		t.Errorf("Expected Server.cache.Length() to be %s, got %s instead",
+			string(expectedResponse.value), cacheLen)
+	}
 }
 
 func TestKeys(t *testing.T) {
+	server := NewServer(nil)
+	serverAddr := "localhost:5000"
+	go func() {
+		if err := server.ListenAndServe(serverAddr); err != nil {
+			t.Errorf("Server failed: %v", err)
+		}
+	}()
+	defer server.Close()
 
+	expectedResponse := response{
+		command: []byte("KEYS"),
+		message: nil,
+		ok:      true,
+		key:     nil,
+		value:   []byte("key1,key2,key3,key4,key5"),
+	}
+
+	server.cache.Set("key1", []byte("value1"))
+	server.cache.Set("key2", []byte("value2"))
+	server.cache.Set("key3", []byte("value3"))
+	server.cache.Set("key4", []byte("value4"))
+	server.cache.Set("key5", []byte("value5"))
+
+	req := request{command: []byte("KEYS")}
+	conn, err := net.Dial("tcp", serverAddr)
+	if err != nil {
+		t.Errorf("Failed to connect to the server: %v", err)
+	}
+	req.write(conn)
+
+	respBuf := [1024]byte{}
+	n, err := conn.Read(respBuf[:])
+	if err != nil {
+		t.Errorf("Error while reading from server")
+	}
+	resp, err := parseResponse(respBuf[:n])
+	if err != nil {
+		t.Logf("Response buffer:\n%s", string(respBuf[:n]))
+		t.Logf("Error while parsing response: %v", err)
+	}
+
+	if resp.ok != expectedResponse.ok {
+		t.Errorf("Expected ok to be \"%v\", got \"%v\" instead",
+			expectedResponse.ok, resp.ok)
+	}
+	if bytes.Compare(resp.command, expectedResponse.command) != 0 {
+		t.Errorf("Expected command to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.command), string(resp.command))
+	}
+	if bytes.Compare(resp.message, expectedResponse.message) != 0 {
+		t.Errorf("Expected message to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.message), string(resp.message))
+	}
+	if bytes.Compare(resp.key, expectedResponse.key) != 0 {
+		t.Errorf("Expected key to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.key), string(resp.key))
+	}
+
+	expectedKeys := strings.Split(string(expectedResponse.value), ",")
+	actualKeys := strings.Split(string(resp.value), ",")
+	if len(expectedKeys) != len(actualKeys) {
+		t.Errorf("Expected to receive %d keys, got %d instead", len(expectedKeys), len(actualKeys))
+	}
+	for i := range expectedKeys {
+		if !strings.Contains(string(resp.value), expectedKeys[i]) {
+			t.Errorf("Key %s not found", expectedKeys[i])
+		}
+	}
 }
 
 func TestPing(t *testing.T) {
+	server := NewServer(nil)
+	serverAddr := "localhost:5000"
+	go func() {
+		if err := server.ListenAndServe(serverAddr); err != nil {
+			t.Errorf("Server failed: %v", err)
+		}
+	}()
+	defer server.Close()
 
+	expectedResponse := response{
+		command: []byte("PING"),
+		message: nil,
+		ok:      true,
+		key:     nil,
+		value:   nil,
+	}
+
+	req := request{command: []byte("PING")}
+	conn, err := net.Dial("tcp", serverAddr)
+	if err != nil {
+		t.Errorf("Failed to connect to the server: %v", err)
+	}
+	req.write(conn)
+
+	respBuf := [1024]byte{}
+	n, err := conn.Read(respBuf[:])
+	if err != nil {
+		t.Errorf("Error while reading from server")
+	}
+	resp, err := parseResponse(respBuf[:n])
+	if err != nil {
+		t.Logf("Response buffer:\n%s", string(respBuf[:n]))
+		t.Logf("Error while parsing response: %v", err)
+	}
+
+	if resp.ok != expectedResponse.ok {
+		t.Errorf("Expected ok to be \"%v\", got \"%v\" instead",
+			expectedResponse.ok, resp.ok)
+	}
+	if bytes.Compare(resp.command, expectedResponse.command) != 0 {
+		t.Errorf("Expected command to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.command), string(resp.command))
+	}
+	if bytes.Compare(resp.message, expectedResponse.message) != 0 {
+		t.Errorf("Expected message to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.message), string(resp.message))
+	}
+	if bytes.Compare(resp.key, expectedResponse.key) != 0 {
+		t.Errorf("Expected key to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.key), string(resp.key))
+	}
+	if bytes.Compare(resp.value, expectedResponse.value) != 0 {
+		t.Errorf("Expected value to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.value), string(resp.value))
+	}
 }
 
 func TestClose(t *testing.T) {
+	server := NewServer(nil)
+	serverAddr := "localhost:5000"
+	go func() {
+		if err := server.ListenAndServe(serverAddr); err != nil {
+			t.Errorf("Server failed: %v", err)
+		}
+	}()
+	defer server.Close()
 
+	expectedResponse := response{
+		command: []byte("CLOSE"),
+		message: nil,
+		ok:      true,
+		key:     nil,
+		value:   nil,
+	}
+
+	req := request{command: []byte("CLOSE")}
+	conn, err := net.Dial("tcp", serverAddr)
+	if err != nil {
+		t.Errorf("Failed to connect to the server: %v", err)
+	}
+	req.write(conn)
+
+	respBuf := [1024]byte{}
+	n, err := conn.Read(respBuf[:])
+	if err != nil {
+		t.Errorf("Error while reading from server")
+	}
+	resp, err := parseResponse(respBuf[:n])
+	if err != nil {
+		t.Logf("Response buffer:\n%s", string(respBuf[:n]))
+		t.Logf("Error while parsing response: %v", err)
+	}
+
+	if resp.ok != expectedResponse.ok {
+		t.Errorf("Expected ok to be \"%v\", got \"%v\" instead",
+			expectedResponse.ok, resp.ok)
+	}
+	if bytes.Compare(resp.command, expectedResponse.command) != 0 {
+		t.Errorf("Expected command to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.command), string(resp.command))
+	}
+	if bytes.Compare(resp.message, expectedResponse.message) != 0 {
+		t.Errorf("Expected message to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.message), string(resp.message))
+	}
+	if bytes.Compare(resp.key, expectedResponse.key) != 0 {
+		t.Errorf("Expected key to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.key), string(resp.key))
+	}
+	if bytes.Compare(resp.value, expectedResponse.value) != 0 {
+		t.Errorf("Expected value to be \"%s\", got \"%s\" instead",
+			string(expectedResponse.value), string(resp.value))
+	}
+
+	one := make([]byte, 1)
+	conn.SetReadDeadline(time.Now().Add(1 * time.Second))
+	_, err = conn.Read(one)
+	if err == nil {
+		t.Error("Expected read from server to fail with EOF after CLOSE but operation succeeded")
+	}
+	if err != io.EOF {
+		t.Errorf("Expected read from server to fail with EOF after CLOSE but got different error: %v", err)
+	}
 }

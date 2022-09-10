27eb6f4c3abe067f9e96dc2685049a3cb5ac00b2
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-09-10 15:37:23 -0400

    misc(map): comments

--- internal/cache/map.go
+++ internal/cache/map.go
@@ -2,21 +2,27 @@ package cache
 
 import "sync"
 
+// CacheMap represents in-memory key-value table safe for concurrent usage.
+// Uses strings as keys. Stores byte slices.
 type CacheMap struct {
 	mu    sync.RWMutex
 	items map[string][]byte
 }
 
+// NewCacheMap returns pointer to initialized CacheMap.
 func NewCacheMap() *CacheMap {
 	return &CacheMap{items: make(map[string][]byte)}
 }
 
+// Set sets given value for the given key, possible overwriting it.
 func (cm *CacheMap) Set(key string, value []byte) {
 	cm.mu.Lock()
 	cm.items[key] = value
 	cm.mu.Unlock()
 }
 
+// Get finds the value for given key. The second return value
+// is a bool that specifies whether the key is present.
 func (cm *CacheMap) Get(key string) (value []byte, ok bool) {
 	cm.mu.RLock()
 	value, ok = cm.items[key]
@@ -24,18 +30,22 @@ func (cm *CacheMap) Get(key string) (value []byte, ok bool) {
 	return value, ok
 }
 
+// Delete removes the key and associated value from the map.
+// If key is not present, Delete is a no-op.
 func (cm *CacheMap) Delete(key string) {
 	cm.mu.Lock()
 	delete(cm.items, key)
 	cm.mu.Unlock()
 }
 
+// Purge removes all keys from the map making it empty.
 func (cm *CacheMap) Purge() {
 	cm.mu.Lock()
 	cm.items = make(map[string][]byte)
 	cm.mu.Unlock()
 }
 
+// Length returns number of items stored in the map.
 func (cm *CacheMap) Length() int {
 	cm.mu.RLock()
 	length := len(cm.items)
@@ -43,6 +53,7 @@ func (cm *CacheMap) Length() int {
 	return length
 }
 
+// Keys returns an array of all keys in the map.
 func (cm *CacheMap) Keys() []string {
 	cm.mu.RLock()
 	keys := make([]string, len(cm.items))

bdd9d38eec9967bd8410414ceef7e33c67124ba8
Nikita Mezhenskyi <nikita.mezhenskyi@gmail.com>
2022-11-14 14:26:40 -0500

    feat(cache): item with expiration

--- internal/cache/item.go
+++ internal/cache/item.go
@@ -1,4 +1,16 @@
 package cache
 
-type Item struct {
+import "time"
+
+// item represents a value stored in cache, it may have an expiration date.
+type item struct {
+	data    []byte
+	expires int64 // if zero, item never expires.
+}
+
+func (i *item) isExpired() bool {
+	if i.expires == 0 {
+		return false
+	}
+	return time.Now().UnixNano() > i.expires
 }
